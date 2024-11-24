461f408cf929eab2486716807d532a4ede634411
Ehab Magdy Abdullah <132620660+EhabMagdyy@users.noreply.github.com>
2024-11-24 20:53:01 +0200

    Update Test.ino

--- Test.ino
+++ Test.ino
@@ -1,23 +1,12 @@
 #include <LittleFS.h>
-#include <Arduino.h>
-#if defined(ESP32) || defined(ARDUINO_RASPBERRY_PI_PICO_W)
+#if defined(ESP32)
 #include <WiFi.h>
 #elif defined(ESP8266)
 #include <ESP8266WiFi.h>
-#elif __has_include(<WiFiNINA.h>)
-#include <WiFiNINA.h>
-#elif __has_include(<WiFi101.h>)
-#include <WiFi101.h>
-#elif __has_include(<WiFiS3.h>)
-#include <WiFiS3.h>
 #endif
 
 #include <Firebase_ESP_Client.h>
-
-// Provide the token generation process info.
 #include <addons/TokenHelper.h>
-
-// Provide the SD card interfaces setting and mounting
 #include <addons/SDHelper.h>
 
 /* 1. Define the WiFi credentials */
@@ -42,20 +31,11 @@ FirebaseConfig config;
 
 bool taskCompleted = false;
 
-#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
-WiFiMulti multi;
-#endif
-
 void setup()
 {
     Serial.begin(115200);
 
-#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
-    multi.addAP(WIFI_SSID, WIFI_PASSWORD);
-    multi.run();
-#else
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-#endif
 
     Serial.print("Connecting to Wi-Fi");
     unsigned long ms = millis();
@@ -63,10 +43,6 @@ void setup()
     {
         Serial.print(".");
         delay(300);
-#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
-        if (millis() - ms > 10000)
-            break;
-#endif
     }
     Serial.println();
     Serial.print("Connected with IP: ");
@@ -81,32 +57,14 @@ void setup()
     /* Assign the user sign in credentials */
     auth.user.email = USER_EMAIL;
     auth.user.password = USER_PASSWORD;
-
-    // The WiFi credentials are required for Pico W
-    // due to it does not have reconnect feature.
-#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
-    config.wifi.clearAP();
-    config.wifi.addAP(WIFI_SSID, WIFI_PASSWORD);
-#endif
-
-    /* Assign the callback function for the long running token generation task */
+    
     config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
-
-    // Comment or pass false value when WiFi reconnection will control by your code or third party library e.g. WiFiManager
     Firebase.reconnectNetwork(true);
-
-    // Since v4.4.x, BearSSL engine was used, the SSL buffer need to be set.
-    // Large data transmission may require larger RX buffer, otherwise connection issue or data read time out can be occurred.
     fbdo.setBSSLBufferSize(4096 /* Rx buffer size in bytes from 512 - 16384 */, 1024 /* Tx buffer size in bytes from 512 - 16384 */);
-
-    /* Assign download buffer size in byte */
-    // Data to be downloaded will read as multiple chunks with this size, to compromise between speed and memory used for buffering.
-    // The memory from external SRAM/PSRAM will not use in the TCP client internal rx buffer.
     config.fcs.download_buffer_size = 2048;
 
     Firebase.begin(&config, &auth);
-
-    // if use SD card, mount it.
+    
     //SD_Card_Mounting(); // See src/addons/SDHelper.h
     pinMode(2, OUTPUT);
 }
@@ -134,6 +92,7 @@ void fcsDownloadCallback(FCS_DownloadStatusInfo info)
 
 void readFile(const char *path)
 {
+  // open file for read
   File file = LittleFS.open(path, "r");
   if(!file)
   {
@@ -143,6 +102,7 @@ void readFile(const char *path)
   Serial.println("Read from file:");
   while(file.available())
   {
+    // printing file data byte by byte in HEX format
     Serial.print(file.read(), HEX);
     Serial.print(' ');
   }
@@ -151,7 +111,6 @@ void readFile(const char *path)
 
 void loop()
 {
-
     // Firebase.ready() should be called repeatedly to handle authentication tasks.
     if (Firebase.ready() && !taskCompleted)
     {
@@ -159,10 +118,9 @@ void loop()
 
         Serial.println("\nDownload file...\n");
 
-        // The file systems for flash and SD/SDMMC can be changed in FirebaseFS.h.
         if (!Firebase.Storage.download(&fbdo, STORAGE_BUCKET_ID /* Firebase Storage bucket id */, "Application.bin" /* path of remote file stored in the bucket */, "/new.bin" /* path to local file */, mem_storage_type_flash /* memory storage type, mem_storage_type_flash and mem_storage_type_sd */, fcsDownloadCallback /* callback function */))
             Serial.println(fbdo.errorReason());
-
+        // Reading the file after it has been downloaded
         readFile("/new.bin");
     }
 }
